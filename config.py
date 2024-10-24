aad76395fdcbee1346224c7144b0f6af1fa5ab34
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2024-10-25 00:17:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+API_TOKENS = [
+    '7274292874:AAE95eVX0BE3LyECLELa7VE5_oA9JXJMDzM',
+    # aur bhi tokens yahan
+]
+CHAT_ID = '-1002217780167'
+USERNAME = "raoxc"
+GROUP_LINK = "https://t.me/raoxc_gc"
