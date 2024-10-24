de3bed8def1384a1467020652d924d335920c196
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2024-10-25 00:20:10 +0530

    Update raid.py

--- raid.py
+++ raid.py
@@ -3,30 +3,10 @@ import time
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from threading import Thread
-
-# API tokens for bots
-API_TOKENS = [
-    '7274292874:AAE95eVX0BE3LyECLELa7VE5_oA9JXJMDzM',
-    '7513832134:AAH-J3ZxQojb_VdZNW-Xgw0PKBSB2wPX6tM',
-    '7202346355:AAGBgVYB2svyQgboCWO6TmwxLV4k_9yq4wI',
-    '6745752993:AAHdggLwLO_gBpY8-YkzupdXYQRxjqPTDWo',
-    '7513768736:AAE9Z4IVoDwT9RwNXBbmXEYFOoX3GXy_RiU',
-    '7424700585:AAFvFK56XF77Lj78c-tzJUpnMN4aVhyiTes',
-    '7045462494:AAH2umwnaF12UFBxwh8sBByt1RMOSRBbYIw',
-    '6942028585:AAEoK0JpC187XRYEiwJX4dx688jcBcoT5y4',
-    '7430127221:AAG3qOqe3j9SbYFvhbWGS7Y_ACprqopOEU8',
-    '7352686451:AAEmwKY9dbEBr5tf5b_NyJhN8oskFqzylYM',
-]
+from config import API_TOKENS, CHAT_ID, USERNAME, GROUP_LINK  # Importing from config
 
 # Initialize bots
 bots = [telebot.TeleBot(token) for token in API_TOKENS]
-
-# Telegram chat ID
-CHAT_ID = '-1002217780167'
-
-# Your Telegram username and group link
-USERNAME = "raoxc"
-GROUP_LINK = "https://t.me/raoxc_gc"
 
 # RAID messages
 RAID_MESSAGES = [
